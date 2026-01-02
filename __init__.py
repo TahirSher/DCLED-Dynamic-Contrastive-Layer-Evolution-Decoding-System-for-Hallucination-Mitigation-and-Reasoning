9097dd6b425550616a0620c0d36ef78ef2c584b8
Tahir Sher <92644572+TahirSher@users.noreply.github.com>
2026-01-02 19:09:25 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,12 +2,12 @@
 __version__ = "1.0.0"
 __author__ = "DCLED Team"
 
-from .model_and_decoding import UnifiedDCSLED
+from .model_and_decoding import UnifiedDCLED
 from .config import get_adaptive_config, get_model_size_category
 from .metrics import MC_calcs, compute_entropy, compute_layer_confidence
 
 __all__ = [
-    'UnifiedDCSLED',
+    'UnifiedDCLED',
     'get_adaptive_config',
     'get_model_size_category',
     'MC_calcs',
@@ -15,3 +15,4 @@ __all__ = [
     'compute_layer_confidence',
 
 ]
+
