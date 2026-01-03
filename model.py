6e36467291728d331cc032798d6c377cc81eb1c1
Tahir Sher <92644572+TahirSher@users.noreply.github.com>
2026-01-03 12:42:00 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from typing import List, Dict, Optional, Tuple, Any
-from utils import stable_softmax, stable_log_softmax, compute_entropy, js_divergence, compute_layer_confidence
+from utils import stable_softmax, stable_log_softmax, compute_entropy, js_divergence, compute_layer_confidence,get_relative_top_filter
 from evolution import EnhancedDCLEDEvolutionEngine, JSLayerSelector, DynamicLayerSignalComputer
 from config import get_model_adaptive_config, get_model_size_category
 from logging_utils import get_logger
@@ -433,6 +433,7 @@ class UnifiedDCLED:
             else:
 
                 raise ValueError(f"Unknown mode: {mode}")
+
 
 
 
